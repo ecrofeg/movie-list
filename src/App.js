1c47d470f721f83154777df9d2b2f271146feab9
ecrofeg <ecrofeg1@gmail.com>
2018-06-12 01:36:32 +0400

    Autocomplete init

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import SearchField from './SearchField';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Hello World
+        <SearchField/>
       </div>
     );
   }
