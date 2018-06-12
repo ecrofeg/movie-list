e07d70f24200ad336fb40b828bad406af12d1843
ecrofeg <ecrofeg1@gmail.com>
2018-06-12 10:44:27 +0400

    Formatting

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 import SearchField from './SearchField';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <SearchField/>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="movie-list">
+				<SearchField/>
+			</div>
+		);
+	}
 }
 
 export default App;
